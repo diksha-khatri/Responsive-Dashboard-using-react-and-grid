4850c9bad17523c9096f30838d2cd92aede6f404
diksha-khatri <dikshakhatri2626@gmail.com>
2022-04-17 00:37:45 +0530

    second commit

--- alpyus-assignment/src/Components/TeamMemberTitle.js
+++ alpyus-assignment/src/Components/TeamMemberTitle.js
@@ -5,7 +5,7 @@ import { BsThreeDots } from "react-icons/bs"
 export default function TeamMemberTitle(){
     return(
         <div className="flex teammember-title">
-                    <h3>Team Members</h3>
+                    <h3 className="margin-zero">Team Members</h3>
                     <div><BsThreeDots /></div>
         </div>
     )
--- alpyus-assignment/src/Components/Title2.js
+++ alpyus-assignment/src/Components/Title2.js
@@ -4,7 +4,7 @@ export default function Title2(){
     return(
         <div className="title2 flex">
         <h2 className="margin-zero">Schedule</h2>
-        <h4 className="margin-zero view-detail small">view detail</h4>
+        <h4 className="margin-zero view-detail small">View detail</h4>
         </div>
     )
 }
